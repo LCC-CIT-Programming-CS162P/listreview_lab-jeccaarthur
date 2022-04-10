6642d44bcde7a3dd2e3a0c90bc6e5fe8b8cbd8f9
jeccaarthur <63317970+jeccaarthur@users.noreply.github.com>
2022-04-10 13:46:50 -0700

    Finished writing reset and update methods

--- Yahtzee/scorecard.py
+++ Yahtzee/scorecard.py
@@ -5,21 +5,51 @@
 import constants
 from playing import clear
 
-# TODO: write resetScorecard and updateScorecard
 def resetScorecard(scorecard):
     """ takes the 2-d list that represents the scorecard as it's parameter and
     sets each of the individual values for the user and the computer to the constant empty.
     The subtotal, bonus and total should be set to 0.
     It does not return a value but the scorecard is altered by the function
     """
-
+    for card in scorecard:
+        card[constants.ONES] = constants.EMPTY
+        card[constants.TWOS] = constants.EMPTY
+        card[constants.THREES] = constants.EMPTY
+        card[constants.FOURS] = constants.EMPTY
+        card[constants.FIVES] = constants.EMPTY
+        card[constants.SIXES] = constants.EMPTY
+        card[constants.THREE_OF_A_KIND] = constants.EMPTY
+        card[constants.FOUR_OF_A_KIND] = constants.EMPTY
+        card[constants.FULL_HOUSE] = constants.EMPTY
+        card[constants.SMALL_STRAIGHT] = constants.EMPTY
+        card[constants.LARGE_STRAIGHT] = constants.EMPTY
+        card[constants.CHANCE] = constants.EMPTY
+        card[constants.YAHTZEE] = constants.EMPTY
+        card[constants.SUBTOTAL] = 0
+        card[constants.BONUS] = 0
+        card[constants.TOTAL] = 0
 
 def updateScorecard(scorecard):
     """ takes the 2-d list that represents the scorecard as it's parameter and
     calculates the subtotal, bonus and total for both the user and the computer.
     It does not return a value but the scorecard is altered by the function
     """
+    for card in scorecard:
+        card[constants.SUBTOTAL] = 0
+        card[constants.BONUS] = 0
+        card[constants.TOTAL] = 0
 
+        # count all scores on card up to subtotal
+        for i in range(constants.SUBTOTAL):
+            if card[i] != constants.EMPTY:
+                card[constants.SUBTOTAL] += card[i]
+
+        # if subtotal is 63 or more, award bonus
+        if card[constants.SUBTOTAL] >= 63:
+            card[constants.BONUS] += 35
+
+        # calculate total
+        card[constants.TOTAL] = card[constants.SUBTOTAL] + card[constants.BONUS]
 
 def formatCell(value):
     return "" if value < 0 else str(value)
